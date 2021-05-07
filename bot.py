6926e2036754bb544c2a666f15947d903215952c
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-05-07 23:23:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 translator = Translator()
-token = 'Your token'
+token = '1721526338:AAEqf3-MMZN7jZworK5q2dxUNQJgKPqWva8'
 bot = Bot(token)
 IN = range(1)
 form = """
